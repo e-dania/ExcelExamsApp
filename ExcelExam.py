046dae50572035c84def137e278e95e88549f185
Antony Wahome <155991781+tonywahome@users.noreply.github.com>
2024-03-30 14:44:55 +0200

    Update ExcelExam.py
    User profile

--- ExcelExam.py
+++ ExcelExam.py
@@ -1,3 +1,4 @@
+#creating a user profile
 def welcome():
     print("Welcome to ExcelExam!")
     print("We will help you assess your strengths and weaknesses and provide a personalized study plan.")
